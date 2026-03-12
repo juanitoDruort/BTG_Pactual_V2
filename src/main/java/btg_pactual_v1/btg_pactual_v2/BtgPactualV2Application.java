4872e269074460b41005911902c2dbfe73af0fbc
Juan Rodriguez <juan.rodriguez@ceiba.com.co>
2026-03-11 20:14:19 -0500

    Inicializar un proyecto Spring Boot con configuración de Gradle y estructura básica de la aplicación

---
+++ src/main/java/btg_pactual_v1/btg_pactual_v2/BtgPactualV2Application.java
@@ -0,0 +1,13 @@
+package btg_pactual_v1.btg_pactual_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtgPactualV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtgPactualV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/btg_pactual_v1/btg_pactual_v2/BtgPactualV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package btg_pactual_v1.btg_pactual_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtgPactualV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
